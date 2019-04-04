dfc667f3b9a10c0582ad3757f221417edcd0078f
mustafa-synectiks <35029729+mustafa-synectiks@users.noreply.github.com>
2019-04-04 13:19:11 +0530

    Attendance files added

---
+++ src/createGraphQLClient.tsx
@@ -0,0 +1,17 @@
+import { ApolloClient, createNetworkInterface } from 'react-apollo';
+
+
+// 	http://dev.apollodata.com/react/initialization.html#creating-client
+export const createGraphQLClient = () => {
+  const networkInterface = createNetworkInterface({
+    uri: 'http://18.234.66.133:8080/graphql'
+
+  });
+  const client = new ApolloClient({
+    networkInterface: networkInterface
+
+  });
+
+  return client;
+};
+
---
+++ src/module.ts
@@ -0,0 +1,13 @@
+import { WelcomePage } from './ui/WelcomePage';
+import { ConfigCtrl } from './ui/Config';
+import { AttendanceOverview } from './ui/AttendanceOverview';
+import { MarkAttendance } from './ui/MarkAttendance';
+import { TeacherAttendance } from './ui/TeacherAttendance';
+
+export {
+  WelcomePage,
+  ConfigCtrl,
+  AttendanceOverview,
+  MarkAttendance,
+  TeacherAttendance,
+}
