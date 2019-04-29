97e5142d3ebb6b2e7b6008315959ed5e69933b2c
Saurabh <saurabh@bel-technology.com>
2019-04-29 14:07:31 +0530

    Student attendance filter code changes

--- src/domain/attendance/TeacherAttendance/TeacherAttendance.tsx
+++ src/domain/attendance/TeacherAttendance/TeacherAttendance.tsx
@@ -2,24 +2,28 @@ import * as React from 'react';
 import DatePicker from 'react-datepicker';
 import * as moment from 'moment';
 import { AttendanceServices } from '../_services';
-import * as ListStudentAttendanceGql from './ListStudentAttendanceQuery.graphql';
-import { withRouter, RouteComponentProps, Link } from 'react-router-dom';
-import { gql, graphql, QueryProps } from "react-apollo";
+import * as StudentAttendanceFilterQueryGql from './StudentAttendanceFilterQuery.graphql';
+import { withRouter, RouteComponentProps } from 'react-router-dom';
+import { graphql, MutationFunc } from "react-apollo";
 import {
-  StudentAttendanceListQuery,
-  StudentAttendanceFragment,
-  StudentAttendanceData,
+  // StudentAttendanceListQuery,
+  DailyStudentAttendanceListQuery,
+  // StudentAttendanceFilterInput,
+  // StudentAttendanceFragment,
+  // StudentAttendanceData,
 } from '../../types';
-import withLoadingHandler from '../../../components/withLoadingHandler';
+// import withLoadingHandler from '../../../components/withLoadingHandler';
+// import { strict } from 'assert';
 
 type StudentAttendanceListPageOwnProps = RouteComponentProps<{}>;
 type StudentAttendanceListPageProps = StudentAttendanceListPageOwnProps & {
-  studentDataListQry: QueryProps & StudentAttendanceListQuery;
+  // studentDataListQry: QueryProps & StudentAttendanceListQuery;
+  mutate: MutationFunc<DailyStudentAttendanceListQuery>;
 };
 
-interface type {
-  checked: boolean;
-}
+// interface type {
+//   checked: boolean;
+// }
 
 class DatePickerComponent extends React.Component<any, any> {
   constructor(props: any) {
@@ -37,7 +41,7 @@ class DatePickerComponent extends React.Component<any, any> {
   }
 
   render() {
-    return <DatePicker selected={this.state.startDate} onChange={this.handleChange} />;
+    return <DatePicker id="dtPicker" value={this.state.startDate} disabled selected={this.state.startDate} onChange={this.handleChange} />;
   }
 }
 
@@ -187,26 +191,26 @@ class TeacherAttendance extends React.Component<StudentAttendanceListPageProps, 
     return subjectsOptions;
   }
   createSemesters(semesters: any){
-    let subjectsOptions = [<option key={0} value="">Select Semester</option>];
+    let semestersOptions = [<option key={0} value="">Select Semester</option>];
     for (let i = 0; i < semesters.length; i++) {
         let id = semesters[i].id;
-        subjectsOptions.push(
+        semestersOptions.push(
             <option key={id} value={id}>{semesters[i].description}</option>
         ); 
     }
-    return subjectsOptions;
+    return semestersOptions;
   }
   createSections(sections: any, selectedBatchId: any) {
-    let batchesOptions = [<option key={0} value="">Select Section</option>];
+    let sectionsOptions = [<option key={0} value="">Select Section</option>];
     for (let i = 0; i < sections.length; i++) {
         let id = sections[i].id;
         if (sections[i].batchId == selectedBatchId) {
-            batchesOptions.push(
+          sectionsOptions.push(
                 <option key={id} value={id}>{sections[i].section}</option>
             );
         }
     }
-    return batchesOptions;
+    return sectionsOptions;
   }
   createLectures(lectures: any, teaches: any, attendanceMasters: any, subjectId: any, teacherId: any, batchId: any, sectionId: any){
     let theachId  = "";
@@ -225,49 +229,70 @@ class TeacherAttendance extends React.Component<StudentAttendanceListPageProps, 
       }
     }
 
-    var todayTime = new Date();
-    var month = todayTime.getMonth() + 1;
-    var ms = month < 10 ? "0"+month : month;
-    var day = todayTime.getDate();
-    var year = todayTime.getFullYear();
-    var dd =  day + "/" + ms + "/" + year; 
-    var dp = new DatePickerComponent(dd);
+    // var todayTime = new Date();
+    // var month = todayTime.getMonth() + 1;
+    // var ms = month < 10 ? "0"+month : month;
+    // var day = todayTime.getDate();
+    // var year = todayTime.getFullYear();
+    // var dd =  day + "-" + ms + "-" + year; 
+    var dd1 = moment(new Date()).format("DD-MM-YYYY");
+    // console.log('moment date dd1 : '+dd1);
+    // var dp = new DatePickerComponent(dd1);
     
-    let subjectsOptions = [<option key={0} value="">Select Lecture</option>];
+    
+    // let dtP1 = document.querySelector("#dtPicker");
+    // console.log("date picker object : ",dtP1);
+
+    let lecturesOptions = [<option key={0} value="">Select Lecture</option>];
     for (let i = 0; i < lectures.length; i++) {
         let id = lectures[i].id;
-        var lcdt = new DatePickerComponent(lectures[i].strLecDate);
-        if(lcdt.props === dp.props && lectures[i].attendancemaster.id === amId){
-          subjectsOptions.push(
+        // var lcdt = new DatePickerComponent(lectures[i].strLecDate);
+        if(lectures[i].strLecDate === dd1 && lectures[i].attendancemaster.id === amId){
+          lecturesOptions.push(
             <option key={id} value={id}>Lecture - {i+1} : {lectures[i].startTime} - {lectures[i].endTime}</option>
-          ); 
+          );
+           
         }
     }
-    return subjectsOptions;
+    
+    return lecturesOptions;
   }
+
+  isCorrectDate(strDate: any){
+    var regex = /^(((0[1-9]|[12]\d|3[01])\/(0[13578]|1[02])\/((19|[2-9]\d)\d{2}))|((0[1-9]|[12]\d|30)\/(0[13456789]|1[012])\/((19|[2-9]\d)\d{2}))|((0[1-9]|1\d|2[0-8])\/02\/((19|[2-9]\d)\d{2}))|(29\/02\/((1[6-9]|[2-9]\d)(0[48]|[2468][048]|[13579][26])|((16|[2468][048]|[3579][26])00))))$/g;
+    return regex.test(strDate) ? true : false;
+  }
+
   onFormSubmit = (e: any) => {
     this.setState({
         submitted: true
     });
-    const { studentDataListQry } = this.props;
+    // const { studentDataListQry } = this.props;
+    const { mutate } = this.props;
     const { studentFilterData } = this.state;
     e.preventDefault();
     if (studentFilterData.branch.id && studentFilterData.department.id &&  studentFilterData.batch.id && studentFilterData.section.id && studentFilterData.lecture.id) {
-      var todayTime = new Date();
-      var month = todayTime.getMonth() + 1;
-      var ms = month < 10 ? "0"+month : month;
-      var day = todayTime.getDate();
-      var year = todayTime.getFullYear();
-      var dd =  day + "/" + ms + "/" + year; 
-      var dp = new DatePickerComponent(dd);
+      // var todayTime = new Date();
+      // var month = todayTime.getMonth() + 1;
+      // var ms = month < 10 ? "0"+month : month;
+      // var day = todayTime.getDate();
+      // var year = todayTime.getFullYear();
+      // var dd =  year+"-"+ms+"-"+day; 
+      // var dp = new DatePickerComponent(dd);
 
-        let dplStudentData = {
-            ...studentFilterData,
+      let dtP = e.target.querySelector("#dtPicker");
+      // if(this.isCorrectDate(dtP.value) === false){
+      //   dtP = moment(new Date()).format("DD-MM-YYYY");
+      // }
+      // console.log("date picker selected values : ", dtP.value);
+      // console.log("moment date : ",   moment(new Date(dtP.value)).format("DD-MM-YYYY"));
+        let studentFilterInputData = {
             branchId: studentFilterData.branch.id,
             departmentId: studentFilterData.department.id,
             batchId: studentFilterData.batch.id,
             sectionId: studentFilterData.section.id,
-            attendanceDate: studentFilterData.dtPicker.dt,
+            subjectId: studentFilterData.subject.id,
+            attendanceDate: dtP.value,
             lectureId: studentFilterData.lecture.id
         };
         
@@ -277,18 +302,19 @@ class TeacherAttendance extends React.Component<StudentAttendanceListPageProps, 
         // delete dplStudentData.section;
         // delete dplStudentData.__typename;
         let btn = e.target.querySelector("button[type='submit']");
-        btn.setAttribute("disabled", true);
-        let dataSavedMessage: any = document.querySelector(".data-saved-message");
-        dataSavedMessage.style.display = "none";
-        return studentDataListQry.refetch({
-            variables: { input: dplStudentData },
+        // btn.setAttribute("disabled", true);
+        // let dataSavedMessage: any = document.querySelector(".data-saved-message");
+        // dataSavedMessage.style.display = "none";
+        return mutate({
+            variables: { filter: studentFilterInputData },
         }).then((data: any) => {
-            btn.removeAttribute("disabled");
-            dataSavedMessage.style.display = "inline-block";
+          console.log('Query Result ', data);
+            // btn.removeAttribute("disabled");
+            // dataSavedMessage.style.display = "inline-block";
             // location.href = `${location.origin}/plugins/ems-student/page/students`;
         }).catch((error: any) => {
-            btn.removeAttribute("disabled");
-            dataSavedMessage.style.display = "inline-block";
+            // btn.removeAttribute("disabled");
+            // dataSavedMessage.style.display = "inline-block";
             console.log('there was an error sending the query result', error);
             return Promise.reject(`Could not retrieve student attendance data: ${error}`);
         });
@@ -372,7 +398,7 @@ class TeacherAttendance extends React.Component<StudentAttendanceListPageProps, 
   }
 
   render() {
-    const { history, match, studentDataListQry } = this.props;
+    const { history, match, mutate } = this.props;
     const { studentFilterData, departments, batches, semesters, subjects, sections, lectures, teaches, attendanceMasters, submitted } = this.state;
     return (
       <section className="plugin-bg-white">
@@ -426,7 +452,7 @@ class TeacherAttendance extends React.Component<StudentAttendanceListPageProps, 
                     </select>
                   </td>
                   <td>
-                    <DatePickerComponent />
+                    <DatePickerComponent disabled />
                   </td>
                   <td>
                     <button className="btn bs" type="submit">Take Attendance</button>
@@ -447,7 +473,7 @@ class TeacherAttendance extends React.Component<StudentAttendanceListPageProps, 
 
 // export default TeacherAttendance;
 export default withRouter(
-  graphql<StudentAttendanceListQuery, StudentAttendanceListPageOwnProps, StudentAttendanceListPageProps>(ListStudentAttendanceGql)(
+  graphql<DailyStudentAttendanceListQuery, StudentAttendanceListPageOwnProps>(StudentAttendanceFilterQueryGql)(
     TeacherAttendance
   )
 );
--- src/domain/types/query-types-generated.ts
+++ src/domain/types/query-types-generated.ts
@@ -1,116 +1,116 @@
 /* tslint:disable */
 //  This file was automatically generated and should not be edited.
 
-export type StudentAttendanceFilterInput = {
-  branchId?: string | null;
-  departmentId?: string | null;
-  batchId?: string | null;
-  sectionId?: string | null;
-  subjectId?: string | null;
-  studentId?: string | null;
-  studentName?: string | null;
-  attendanceDate?: string | null;
-  lectureId?: string | null;
-};
+// export type StudentAttendanceFilterInput = {
+//   branchId?: string | null;
+//   departmentId?: string | null;
+//   batchId?: string | null;
+//   sectionId?: string | null;
+//   subjectId?: string | null;
+//   studentId?: string | null;
+//   studentName?: string | null;
+//   attendanceDate?: string | null;
+//   lectureId?: string | null;
+// };
 
-export type DailyAttendanceVo = {
-  studentId?: string | null;
-  studentName?: string | null;
-  currentDateStatus?: string | null;
-  previousOneDayStatus?: string | null;
-  previousTwoDayStatus?: string | null;
-  previousThreeDayStatus?: string | null;
-  comments?: string | null;
-};
+// export type DailyAttendanceVo = {
+//   studentId?: string | null;
+//   studentName?: string | null;
+//   currentDateStatus?: string | null;
+//   previousOneDayStatus?: string | null;
+//   previousTwoDayStatus?: string | null;
+//   previousThreeDayStatus?: string | null;
+//   comments?: string | null;
+// };
 
-export type AddStudentAttendanceInput = {
-  id?: number | null;
-  attendanceStatus?: string | null;
-  comments?: string | null;
-  student: {
-    studentName?: any | null;
-  };
-  lecture: {
-    id?: any | null;
-  };
-};
+// export type AddStudentAttendanceInput = {
+//   id?: number | null;
+//   attendanceStatus?: string | null;
+//   comments?: string | null;
+//   student: {
+//     studentName?: any | null;
+//   };
+//   lecture: {
+//     id?: any | null;
+//   };
+// };
 
-export type AddStudentAttendanceMutationVariables = {
-  input: AddStudentAttendanceInput;
-};
+// export type AddStudentAttendanceMutationVariables = {
+//   input: AddStudentAttendanceInput;
+// };
 
-export type AddStudentAttendanceMutation = {
-  addStudentAttendance: {
-    studentAttendance: {
-      id: number;
-      attendanceStatus: string;
-      comments: string;
-      student: {
-        studentName: any;
-      };
-      lecture: {
-        id: any;
-      };
-    };
-  };
-};
+// export type AddStudentAttendanceMutation = {
+//   addStudentAttendance: {
+//     studentAttendance: {
+//       id: number;
+//       attendanceStatus: string;
+//       comments: string;
+//       student: {
+//         studentName: any;
+//       };
+//       lecture: {
+//         id: any;
+//       };
+//     };
+//   };
+// };
 
 /* Location */
 
-export type locationListQuery = {
-  locations: Array<{
-    id: string;
-    name: string;
-    address: string;
-    appliesTo: string;
-  }>;
-};
+// export type locationListQuery = {
+//   locations: Array<{
+//     id: string;
+//     name: string;
+//     address: string;
+//     appliesTo: string;
+//   }>;
+// };
 
-export type locationQueryVariables = {
-  locationId: any;
-};
+// export type locationQueryVariables = {
+//   locationId: any;
+// };
 
-export type locationQuery = {
-  location: {
-    id: any;
-    name: string;
-    address: string;
-    appliesTo: string;
-  };
-};
+// export type locationQuery = {
+//   location: {
+//     id: any;
+//     name: string;
+//     address: string;
+//     appliesTo: string;
+//   };
+// };
 
-export type locationDetailsFragment = {
-  id: any;
-  name: string;
-  address: string;
-  appliesTo: string;
-};
+// export type locationDetailsFragment = {
+//   id: any;
+//   name: string;
+//   address: string;
+//   appliesTo: string;
+// };
 
-export type locationSummaryFragment = {
-  id: any;
-  name: string;
-  address: string;
-  appliesTo: string;
-};
+// export type locationSummaryFragment = {
+//   id: any;
+//   name: string;
+//   address: string;
+//   appliesTo: string;
+// };
 
 /* StudentAttendance */
 
-export type StudentAttendanceListQuery = {
-  studentAttendances: Array<{
-    id: number;
-    attendanceStatus: string;
-    comments: string;
-    student: {
-      studentName: any;
-    };
-    lecture: {
-      id: any;
-    };
-  }>;
-};
+// export type StudentAttendanceListQuery = {
+//   studentAttendances: Array<{
+//     id: number;
+//     attendanceStatus: string;
+//     comments: string;
+//     student: {
+//       studentName: any;
+//     };
+//     lecture: {
+//       id: any;
+//     };
+//   }>;
+// };
 
-export type StudentDailyAttendanceListQuery = {
-  getDailyStudentAttendance: Array<{
+export type DailyStudentAttendanceListQuery = {
+  getDailyStudentAttendanceData: Array<{
     studentId: number;
     studentName: string;
     currentDateStatus: string;
@@ -121,100 +121,100 @@ export type StudentDailyAttendanceListQuery = {
   }>;
 };
 
-export type StudentDailyAttendanceFragment = {
-  studentId: number;
-  studentName: string;
-  currentDateStatus: string;
-  previousOneDayStatus: string;
-  previousTwoDayStatus: string;
-  previousThreeDayStatus: string;
-  comments: string;
-};
+// export type DailyStudentAttendanceDetailsFragment = {
+//   studentId: number;
+//   studentName: string;
+//   currentDateStatus: string;
+//   previousOneDayStatus: string;
+//   previousTwoDayStatus: string;
+//   previousThreeDayStatus: string;
+//   comments: string;
+// };
 
-export type StudentAttendanceQueryVariables = {
-  studentAttendanceId: number;
-};
+// export type StudentAttendanceQueryVariables = {
+//   studentAttendanceId: number;
+// };
 
-export type StudentAttendanceQuery = {
-  studentAttendance: {
-    id: number;
-    attendanceStatus: string;
-    comments: string;
-    student: {
-      studentName: any;
-    };
-    lecture: {
-      id: any;
-    };
-  };
-};
+// export type StudentAttendanceQuery = {
+//   studentAttendance: {
+//     id: number;
+//     attendanceStatus: string;
+//     comments: string;
+//     student: {
+//       studentName: any;
+//     };
+//     lecture: {
+//       id: any;
+//     };
+//   };
+// };
 
-export type StudentAttendanceFragment = {
-  id: number;
-  attendanceStatus: string;
-  comments: string;
-  student: {
-    studentName: any;
-  };
-  lecture: {
-    id: any;
-  };
-};
+// export type StudentAttendanceFragment = {
+//   id: number;
+//   attendanceStatus: string;
+//   comments: string;
+//   student: {
+//     studentName: any;
+//   };
+//   lecture: {
+//     id: any;
+//   };
+// };
 
-export type StudentAttendanceDetailsFragment = {
-  id: number;
-  attendanceStatus: string;
-  comments: string;
-  student: {
-    studentName: any;
-  };
-  lecture: {
-    id: any;
-  };
-};
+// export type StudentAttendanceDetailsFragment = {
+//   id: number;
+//   attendanceStatus: string;
+//   comments: string;
+//   student: {
+//     studentName: any;
+//   };
+//   lecture: {
+//     id: any;
+//   };
+// };
 
-export type StudentAttendanceSummaryFragment = {
-  id: number;
-  attendanceStatus: string;
-  comments: string;
-  student: {
-    studentName: any;
-  };
-  lecture: {
-    id: any;
-  };
-};
+// export type StudentAttendanceSummaryFragment = {
+//   id: number;
+//   attendanceStatus: string;
+//   comments: string;
+//   student: {
+//     studentName: any;
+//   };
+//   lecture: {
+//     id: any;
+//   };
+// };
 
-export type UpdateStudentAttendanceInput = {
-  id: number;
-  attendanceStatus?: string | null;
-  comments?: string | null;
-  student: {
-    studentName?: any | null;
-  };
-  lecture: {
-    id?: any | null;
-  };
-};
+// export type UpdateStudentAttendanceInput = {
+//   id: number;
+//   attendanceStatus?: string | null;
+//   comments?: string | null;
+//   student: {
+//     studentName?: any | null;
+//   };
+//   lecture: {
+//     id?: any | null;
+//   };
+// };
 
-export type UpdateStudentAttendanceMutationVariables = {
-  input: UpdateStudentAttendanceInput;
-};
+// export type UpdateStudentAttendanceMutationVariables = {
+//   input: UpdateStudentAttendanceInput;
+// };
 
-export type UpdateStudentAttendanceMutation = {
-  updateStudentAttendance: {
-    studentAttendance: {
-      id: number;
-      attendanceStatus: string;
-      comments: string;
-      student: {
-        studentName: any;
-      };
-      lecture: {
-        id: any;
-      };
-    };
-  };
-};
+// export type UpdateStudentAttendanceMutation = {
+//   updateStudentAttendance: {
+//     studentAttendance: {
+//       id: number;
+//       attendanceStatus: string;
+//       comments: string;
+//       student: {
+//         studentName: any;
+//       };
+//       lecture: {
+//         id: any;
+//       };
+//     };
+//   };
+// };
 
 /* tslint:enable */
